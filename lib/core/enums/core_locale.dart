d321709468bace024c5e2f947a9c1983db7d7777
önder <dasbey@hotmail.com>
2024-03-25 16:37:13 +0300

    Removed ipa file

---
+++ lib/core/enums/core_locale.dart
@@ -0,0 +1,39 @@
+import 'package:dash_flags/dash_flags.dart';
+import 'package:flutter/material.dart';
+
+enum CoreLocale {
+  en(Locale('en', 'US')),
+  ar(Locale('ar', 'AR')),
+  tr(Locale('tr', 'TR'));
+
+  final Locale locale;
+  // ignore: sort_constructors_first
+  const CoreLocale(this.locale);
+
+  static final List<Locale> supportedLocales =
+      CoreLocale.values.map((e) => e.locale).toList();
+
+  Country get flag {
+    switch (this) {
+      case CoreLocale.en:
+        return Country.us;
+      case CoreLocale.tr:
+        return Country.tr;
+      case CoreLocale.ar:
+        return Country.sa;
+    }
+  }
+
+  static CoreLocale fromLocale(Locale locale) {
+    switch (locale.languageCode) {
+      case 'en':
+        return CoreLocale.en;
+      case 'tr':
+        return CoreLocale.tr;
+      case 'ar':
+        return CoreLocale.ar;
+      default:
+        throw Exception('Unsupported locale: $locale');
+    }
+  }
+}
